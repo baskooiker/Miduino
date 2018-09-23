28866d4b713c95c024f9016c07b2de1a76fcc42b
Bas Kooiker <baskooiker90@gmail.com>
2018-09-23 22:41:34 +0200

    removed build files

--- MiduinoSketch/MiduinoSketch.ino
+++ MiduinoSketch/MiduinoSketch.ino
@@ -75,177 +75,180 @@ void all_notes_off(uint8_t* storage, uint8_t channel)
 }
 
 void loop() {
-  if (MIDI.read())
-  {
-    switch (MIDI.getType())
-    {
-      case midi::MidiType::Clock:
-        counter = (counter + 1) % TICKS_PER_STEP;
-        if (counter == 0)
-        {
-          play_step = true;
-        }
-        break;
-      case midi::MidiType::Stop:
-//        stop_notes();
-        data.step = 0;
-        break;
-      case midi::MidiType::ControlChange:
-        switch (MIDI.getData1())
-        {
-          case BSP_KNOB_08:
-              data.rocket_density = (MIDI.getData2() / 127.) * .8f + .2f;
-              break;
-          case BSP_STEP_01:
-              if (MIDI.getData2() == 0)
-              {
-                  randomize_503_sound();
-              }
-              break;
-          case BSP_STEP_02:
-              if (MIDI.getData2() == 0)
-              {
-                  randomize_503_seq(&data);
-              }
-              break;
-          case BSP_STEP_03:
-              randomize_522_seq(&data);
-              break;
-          case BSP_STEP_15:
-          case BSP_STEP_16:
-            if (MIDI.getData2() == 0)
-            {
-                randomize_rocket_seq(&data);
-            }
-            break;
-          default:
-            break;
-        }
-        break;
-      case midi::MidiType::NoteOn:
-        switch (MIDI.getData1())
-        {
-          case BSP_PAD_01:
-            data.bsp_pad_01_down = true;
-            break;
-          case BSP_PAD_02:
-            data.bsp_pad_02_down = true;
-            break;
-          case BSP_PAD_03:
-            data.bsp_pad_03_down = true;
-            break;
-          case BSP_PAD_04:
-            data.bsp_pad_04_down = true;
-            break;
-          case BSP_PAD_05:
-            data.bsp_pad_05_down = true;
-            break;
-          case BSP_PAD_06:
-            data.bsp_pad_06_down = true;
-            break;
-          case BSP_PAD_07:
-            data.bsp_pad_07_down = true;
-            break;
-          case BSP_PAD_08:
-            data.bsp_pad_08_down = true;
-            break;
-          case BSP_PAD_09:
-            data.bsp_pad_09_down = true;
-            break;
-          case BSP_PAD_10:
-            data.bsp_pad_10_down = true;
-            break;
-          case BSP_PAD_11:
-            data.bsp_pad_11_down = true;
-            break;
-          case BSP_PAD_12:
-            data.bsp_pad_12_down = true;
-            break;
-          case BSP_PAD_13:
-            data.bsp_pad_13_down = true;
-            break;
-          case BSP_PAD_14:
-            data.bsp_pad_14_down = true;
-            break;
-          case BSP_PAD_15:
-            data.bsp_pad_15_down = true;
-            break;
-          case BSP_PAD_16:
-            data.bsp_pad_16_down = true;
-            break;
-          default:
-            break;
-        }
-        break;
-      case midi::MidiType::NoteOff:
-        switch (MIDI.getData1())
-        {
-          case BSP_PAD_01:
-            data.bsp_pad_01_down = false;
-            break;
-          case BSP_PAD_02:
-            data.bsp_pad_02_down = false;
-            break;
-          case BSP_PAD_03:
-            data.bsp_pad_03_down = false;
-            break;
-          case BSP_PAD_04:
-            data.bsp_pad_04_down = false;
-            break;
-          case BSP_PAD_05:
-            data.bsp_pad_05_down = false;
-            break;
-          case BSP_PAD_06:
-            data.bsp_pad_06_down = false;
-            break;
-          case BSP_PAD_07:
-            data.bsp_pad_07_down = false;
-            break;
-          case BSP_PAD_08:
-            data.bsp_pad_08_down = false;
-            break;
-          case BSP_PAD_09:
-            data.bsp_pad_09_down = false;
-            break;
-          case BSP_PAD_10:
-            data.bsp_pad_10_down = false;
-            break;
-          case BSP_PAD_11:
-            data.bsp_pad_11_down = false;
-            break;
-          case BSP_PAD_12:
-            data.bsp_pad_12_down = false;
-            break;
-          case BSP_PAD_13:
-            data.bsp_pad_13_down = false;
-            break;
-          case BSP_PAD_14:
-            data.bsp_pad_14_down = false;
-            break;
-          case BSP_PAD_15:
-            data.bsp_pad_15_down = false;
-            break;
-          case BSP_PAD_16:
-            data.bsp_pad_16_down = false;
-            break;
-          default:
-            break;
-        }
-        break;
-      default:
-        break;
-    }
-  }
+	if (MIDI.read())
+	{
+		switch (MIDI.getType())
+	    {
+		    case midi::MidiType::Clock:
+		        counter = (counter + 1) % TICKS_PER_STEP;
+		        if (counter == 0)
+		        {
+			        play_step = true;
+		        }
+		    break;
+		    case midi::MidiType::Stop:
+                all_notes_off(data.storage_503, MIDI_CHANNEL_503);
+                all_notes_off(data.storage_522, MIDI_CHANNEL_522);
+                all_notes_off(data.storage_p50, MIDI_CHANNEL_P50);
+                all_notes_off(data.storage_rocket, MIDI_CHANNEL_ROCKET);
+		        data.step = 0;
+		    break;
+		    case midi::MidiType::ControlChange:
+		        switch (MIDI.getData1())
+		        {
+			        case BSP_KNOB_08:
+				        data.rocket_density = (MIDI.getData2() / 127.) * .8f + .2f;
+				        break;
+			        case BSP_STEP_01:
+				        if (MIDI.getData2() == 0)
+				        {
+					        randomize_503_sound();
+				        }
+				        break;
+			        case BSP_STEP_02:
+				        if (MIDI.getData2() == 0)
+				        {
+					        randomize_503_seq(&data);
+				        }
+				        break;
+			        case BSP_STEP_03:
+				        randomize_522_seq(&data);
+				        break;
+			        case BSP_STEP_15:
+			        case BSP_STEP_16:
+			        if (MIDI.getData2() == 0)
+			        {
+				        randomize_rocket_seq(&data);
+			        }
+			        break;
+			        default:
+			        break;
+		        }
+		    break;
+		    case midi::MidiType::NoteOn:
+			    switch (MIDI.getData1())
+			    {
+				    case BSP_PAD_01:
+				    data.bsp_pad_01_down = true;
+				    break;
+				    case BSP_PAD_02:
+				    data.bsp_pad_02_down = true;
+				    break;
+				    case BSP_PAD_03:
+				    data.bsp_pad_03_down = true;
+				    break;
+				    case BSP_PAD_04:
+				    data.bsp_pad_04_down = true;
+				    break;
+				    case BSP_PAD_05:
+				    data.bsp_pad_05_down = true;
+				    break;
+				    case BSP_PAD_06:
+				    data.bsp_pad_06_down = true;
+				    break;
+				    case BSP_PAD_07:
+				    data.bsp_pad_07_down = true;
+				    break;
+				    case BSP_PAD_08:
+				    data.bsp_pad_08_down = true;
+				    break;
+				    case BSP_PAD_09:
+				    data.bsp_pad_09_down = true;
+				    break;
+				    case BSP_PAD_10:
+				    data.bsp_pad_10_down = true;
+				    break;
+				    case BSP_PAD_11:
+				    data.bsp_pad_11_down = true;
+				    break;
+				    case BSP_PAD_12:
+				    data.bsp_pad_12_down = true;
+				    break;
+				    case BSP_PAD_13:
+				    data.bsp_pad_13_down = true;
+				    break;
+				    case BSP_PAD_14:
+				    data.bsp_pad_14_down = true;
+				    break;
+				    case BSP_PAD_15:
+				    data.bsp_pad_15_down = true;
+				    break;
+				    case BSP_PAD_16:
+				    data.bsp_pad_16_down = true;
+				    break;
+				    default:
+		    	    break;
+			    }
+			    break;
+		    case midi::MidiType::NoteOff:
+			    switch (MIDI.getData1())
+			    {
+				    case BSP_PAD_01:
+				    data.bsp_pad_01_down = false;
+				    break;
+				    case BSP_PAD_02:
+				    data.bsp_pad_02_down = false;
+				    break;
+				    case BSP_PAD_03:
+				    data.bsp_pad_03_down = false;
+				    break;
+				    case BSP_PAD_04:
+				    data.bsp_pad_04_down = false;
+				    break;
+				    case BSP_PAD_05:
+				    data.bsp_pad_05_down = false;
+				    break;
+				    case BSP_PAD_06:
+				    data.bsp_pad_06_down = false;
+				    break;
+				    case BSP_PAD_07:
+				    data.bsp_pad_07_down = false;
+				    break;
+				    case BSP_PAD_08:
+				    data.bsp_pad_08_down = false;
+				    break;
+				    case BSP_PAD_09:
+				    data.bsp_pad_09_down = false;
+				    break;
+				    case BSP_PAD_10:
+				    data.bsp_pad_10_down = false;
+				    break;
+				    case BSP_PAD_11:
+				    data.bsp_pad_11_down = false;
+				    break;
+				    case BSP_PAD_12:
+				    data.bsp_pad_12_down = false;
+				    break;
+				    case BSP_PAD_13:
+				    data.bsp_pad_13_down = false;
+				    break;
+				    case BSP_PAD_14:
+				    data.bsp_pad_14_down = false;
+				    break;
+				    case BSP_PAD_15:
+				    data.bsp_pad_15_down = false;
+				    break;
+				    case BSP_PAD_16:
+				    data.bsp_pad_16_down = false;
+				    break;
+				    default:
+				    break;
+			    }
+			    break;
+		    default:
+		    break;
+	    }
+	}
 
-  if (play_step)
-  {
-    play_503(&data);
-    play_522(&data);
-    play_rocket(&data);
-    play_P50(&data);
+	if (play_step)
+	{
+        play_503(&data);
+        play_522(&data);
+        play_rocket(&data);
+        play_P50(&data);
 
-    play_step = false;
-    data.step = (data.step + 1) % COMMON_DENOMINATOR;
-  }
+        play_step = false;
+        data.step = (data.step + 1) % COMMON_DENOMINATOR;
+	}
 }
 
