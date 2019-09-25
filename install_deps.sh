de845e6eeef910a22bfddb7f5726c93f1a79fb7e
Bas Kooiker <baskooiker90@gmail.com>
2019-09-25 21:20:10 +0200

    updates for VS19

---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+cd C:\OF\of_v0.10.1_vs2017_release\addons
+git clone https://github.com/danomatika/ofxMidi.git
+git clone https:/github.com/npisanti/ofxSIMDFloats.git
+git clone https://github.com/mirrorboy714/ofxBpm.git
+git clone https://github.com/npisanti/ofxAudioFile.git
+git clone https://github.com/npisanti/ofxPDSP.git
